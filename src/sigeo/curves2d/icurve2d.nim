f30a1351e2a27072fcb464cecc7b16168b0d7ec1
levovix <xlevovix@gmail.com>
2026-05-08 21:53:41 +0300

    add: makeInterface macro

--- src/sigeo/curves2d/icurve2d.nim
+++ src/sigeo/curves2d/icurve2d.nim
@@ -1,78 +1,28 @@
-import std/hashes
 import ../core/[vectors, points]
 import ../macros/[interfaces]
 import ./[lineSection, circleArc]
 
-type
-  VtableCurve2d* = object
-    typenameHash*: Hash
-    
-    destroy*: proc(this: pointer) {.nimcall, raises: [].}
-    trace*: proc(this: pointer, env: pointer) {.nimcall, raises: [].}
-    dup*: proc(this: var pointer, other: pointer) {.nimcall, raises: [].}
-    sink*: proc(this: var pointer, other: pointer) {.nimcall, raises: [].}
-    
-    destroyRef*: proc(this: pointer) {.nimcall, raises: [].}
-    traceRef*: proc(this: pointer, env: pointer) {.nimcall, raises: [].}
-    dupRef*: proc(this: var pointer, other: pointer) {.nimcall, raises: [].}
-    copyRef*: proc(this: var pointer, other: pointer) {.nimcall, raises: [].}
 
-    length*: proc(this: pointer;): Float {.nimcall.}
-    pointAtParam*: proc(this: pointer; param: FloatParam): Point2 {.nimcall.}
-  
-  Curve2d* = object
-    ## untraced pointer to Curve2d, should be used in function arguments
-    vtable*: ptr VtableCurve2d
-    obj*: pointer
-  
-  OwnedCurve2d* = object
-    ## acts like a unique_ptr in C++, should be used in object fields if it is NOT intended to be shared
-    vtable*: ptr VtableCurve2d
-    obj*: pointer
-  
-  RefCurve2d* = object
-    ## acts like a ref object, should be used in object fields if it IS intended to be shared
-    vtable*: ptr VtableCurve2d
-    obj*: pointer
+makeInterface Curve2d:
+  proc length(this;): Float
+  proc pointAtParam(this; param: FloatParam): Point2
 
 
-proc length*(this: Curve2d;): Float {.inline.} = this.vtable.length(this.obj)
-proc pointAtParam*(this: Curve2d; param: FloatParam): Point2 {.inline.} = this.vtable.pointAtParam(this.obj, param)
+Curve2d.implementInterfaceFor(LineSection, CircleArc)
 
 
-proc `=destroy`(this: OwnedCurve2d) {.inline.} =
-  this.vtable.destroy(this.obj)
+when isMainModule:
+  let line = lineSection(point2(0, 0), point2(2, 0))
+  let circle = circleArc(point2(4, 0), 2)
 
-proc `=trace`(this: var OwnedCurve2d, env: pointer) {.inline.} =
-  this.vtable.trace(this.obj, env)
+  let arr = @[Curve2d line, circle]
 
-proc `=copy`(this: var OwnedCurve2d, other: OwnedCurve2d) {.inline.} =
-  this.vtable.destroy(this.obj)
-  this.vtable.dup(this.obj, other.obj)
-
-proc `=dup`(this: OwnedCurve2d): OwnedCurve2d {.inline.} =
-  this.vtable.dup(result.obj, this.obj)
-
-proc `=sink`(this: var OwnedCurve2d, other: OwnedCurve2d) {.inline.} =
-  this.vtable.sink(this.obj, other.obj)
-
-
-proc `=destroy`(this: RefCurve2d) {.inline.} = this.vtable.destroyRef(this.obj)
-proc `=trace`(this: var RefCurve2d, env: pointer) {.inline.} = this.vtable.traceRef(this.obj, env)
-
-
-converter asPtr*(this: OwnedCurve2d): Curve2d = cast[Curve2d](this)
-converter asPtr*(this: RefCurve2d): Curve2d = cast[Curve2d](this)
-
-
-
-when false:
-  # todo
-
-  makeInterface Curve2d:
-    proc length(this;): Float
-    proc pointAtParam(this; param: FloatParam): Point2
-    # proc derAtParam(this; param: FloatParam): Vec2
-
-  Curve2d.implementInterfaceFor(LineSection, CircleArc)
+  for x in arr:
+    echo "[", x.vtable.typenameHash, "]"
+    echo "length = ", x.length
+    echo "pointAtParam(0) = ", x.pointAtParam(0)
+    echo "pointAtParam(0.25) = ", x.pointAtParam(0.25)
+    echo "pointAtParam(0.5) = ", x.pointAtParam(0.5)
+    echo "pointAtParam(0.75) = ", x.pointAtParam(0.75)
+    echo "pointAtParam(1) = ", x.pointAtParam(1)
 
--- src/sigeo/macros/interfaces.nim
+++ src/sigeo/macros/interfaces.nim
@@ -1,21 +1,378 @@
-import std/[macros]
+import std/[macros, hashes]
+
+type
+  MethodSig = object
+    name: string
+    nonThisParams: seq[NimNode]  # IdentDefs with original types (no pointer substitution)
+    retType: NimNode
+
+
+proc publicField(name: string, typ: NimNode): NimNode =
+  nnkIdentDefs.newTree(
+    nnkPostfix.newTree(ident"*", ident(name)),
+    typ,
+    newEmptyNode()
+  )
+
+
+proc procTy(params: seq[NimNode], retType: NimNode, raisesNone: bool): NimNode =
+  var formalParams = @[retType]
+  formalParams.add(params)
+  var pragma = nnkPragma.newTree(ident"nimcall")
+  if raisesNone:
+    pragma.add nnkExprColonExpr.newTree(ident"raises", nnkBracket.newTree())
+  nnkProcTy.newTree(nnkFormalParams.newTree(formalParams), pragma)
 
 
 macro makeInterface*(name: untyped, body: untyped) =
-  nnkTypeSection.newTree(
+  let nameStr = name.strVal
+  let vtableName = ident("Vtable" & nameStr)
+
+  proc lifecycleProc(fname: string, params: seq[NimNode]): NimNode =
+    publicField(fname, procTy(params, newEmptyNode(), raisesNone=true))
+
+  var vtableFields = nnkRecList.newTree()
+  vtableFields.add publicField("typenameHash", bindSym"Hash")
+
+  vtableFields.add lifecycleProc("destroy", @[
+    newIdentDefs(ident("this"), bindSym("pointer"))
+  ])
+  vtableFields.add lifecycleProc("trace", @[
+    newIdentDefs(ident("this"), bindSym("pointer")),
+    newIdentDefs(ident"env", bindSym("pointer"))
+  ])
+  # dup/sink: this=source (ptr), other=dest (var ptr)
+  vtableFields.add lifecycleProc("dup", @[
+    newIdentDefs(ident("this"), bindSym("pointer")),
+    newIdentDefs(ident"other", nnkVarTy.newTree(bindSym("pointer")))
+  ])
+  vtableFields.add lifecycleProc("sink", @[
+    newIdentDefs(ident("this"), bindSym("pointer")),
+    newIdentDefs(ident"other", nnkVarTy.newTree(bindSym("pointer")))
+  ])
+
+  # Collect methods while building vtable fields
+  var methods: seq[MethodSig]
+
+  for stmt in body:
+    if stmt.kind == nnkProcDef:
+      let methodName = $stmt[0]
+      let formalParams = stmt[3]
+      let retType = formalParams[0]
+
+      var vtableParams: seq[NimNode]
+      var nonThisParams: seq[NimNode]
+      for i in 1 ..< formalParams.len:
+        let p = formalParams[i]
+        if p[1].kind == nnkEmpty:
+          vtableParams.add(nnkIdentDefs.newTree(p[0], ident"pointer", newEmptyNode()))
+        else:
+          vtableParams.add(nnkIdentDefs.newTree(p[0], p[1], newEmptyNode()))
+          nonThisParams.add(nnkIdentDefs.newTree(p[0], p[1], newEmptyNode()))
+
+      vtableFields.add(publicField(methodName, procTy(vtableParams, retType, false)))
+      methods.add(MethodSig(name: methodName, nonThisParams: nonThisParams, retType: retType))
+
+  # Xxx / OwnedXxx share the same shape
+  proc wrapperType(typName: NimNode): NimNode =
     nnkTypeDef.newTree(
-      name,
+      nnkPostfix.newTree(ident"*", typName),
       newEmptyNode(),
       nnkObjectTy.newTree(
         newEmptyNode(),
         newEmptyNode(),
-        newEmptyNode()
+        nnkRecList.newTree(
+          publicField("vtable", nnkPtrTy.newTree(ident("Vtable" & nameStr))),
+          publicField("obj", ident"pointer")
+        )
       )
+    )
+
+  let typeSection = nnkTypeSection.newTree(
+    nnkTypeDef.newTree(
+      nnkPostfix.newTree(ident"*", vtableName),
+      newEmptyNode(),
+      nnkObjectTy.newTree(newEmptyNode(), newEmptyNode(), vtableFields)
+    ),
+    wrapperType(name),
+    wrapperType(ident("Owned" & nameStr))
+  )
+
+  # --- Proc generation ---
+
+  let ownedName = ident("Owned" & nameStr)
+
+  proc mkProc(procName: NimNode, params: seq[NimNode], body: NimNode): NimNode =
+    nnkProcDef.newTree(
+      procName, newEmptyNode(), newEmptyNode(),
+      nnkFormalParams.newTree(params),
+      nnkPragma.newTree(ident"inline"), newEmptyNode(), body
+    )
+
+  proc mkConverter(convName: NimNode, params: seq[NimNode], body: NimNode): NimNode =
+    nnkConverterDef.newTree(
+      convName, newEmptyNode(), newEmptyNode(),
+      nnkFormalParams.newTree(params),
+      newEmptyNode(), newEmptyNode(), body
+    )
+
+  proc dot(obj, field: string): NimNode = nnkDotExpr.newTree(ident(obj), ident(field))
+
+  proc callThrough(obj, methodName: string, args: seq[NimNode]): NimNode =
+    result = nnkCall.newTree(
+      nnkDotExpr.newTree(nnkDotExpr.newTree(ident(obj), ident"vtable"), ident(methodName))
+    )
+    for a in args: result.add(a)
+
+  result = nnkStmtList.newTree(typeSection)
+
+  # Method forwarders on the base (Xxx) type
+  for m in methods:
+    var fparams: seq[NimNode] = @[m.retType, newIdentDefs(ident"this", name)]
+    var callArgs: seq[NimNode] = @[dot("this", "obj")]
+    for p in m.nonThisParams:
+      fparams.add(nnkIdentDefs.newTree(p[0], p[1], newEmptyNode()))
+      callArgs.add(p[0])
+    result.add mkProc(
+      nnkPostfix.newTree(ident"*", ident(m.name)),
+      fparams,
+      newStmtList(callThrough("this", m.name, callArgs))
+    )
+
+  # OwnedXxx lifecycle hooks
+  result.add mkProc(
+    nnkAccQuoted.newTree(ident"=destroy"),
+    @[newEmptyNode(), newIdentDefs(ident"this", ownedName)],
+    newStmtList(callThrough("this", "destroy", @[dot("this", "obj")]))
+  )
+  result.add mkProc(
+    nnkAccQuoted.newTree(ident"=trace"),
+    @[newEmptyNode(), newIdentDefs(ident"this", nnkVarTy.newTree(ownedName)),
+      newIdentDefs(ident"env", ident"pointer")],
+    newStmtList(callThrough("this", "trace", @[dot("this", "obj"), ident"env"]))
+  )
+  # =copy: destroy this, then dup from other (source) into this (dest)
+  result.add mkProc(
+    nnkAccQuoted.newTree(ident"=copy"),
+    @[newEmptyNode(), newIdentDefs(ident"this", nnkVarTy.newTree(ownedName)),
+      newIdentDefs(ident"other", ownedName)],
+    newStmtList(
+      callThrough("this", "destroy", @[dot("this", "obj")]),
+      callThrough("other", "dup", @[dot("other", "obj"), dot("this", "obj")]),
+      nnkAsgn.newTree(dot("this", "vtable"), dot("other", "vtable"))
+    )
+  )
+  # =dup: set vtable, then dup from this (source) into result (dest)
+  result.add mkProc(
+    nnkAccQuoted.newTree(ident"=dup"),
+    @[ownedName, newIdentDefs(ident"this", ownedName)],
+    newStmtList(
+      nnkAsgn.newTree(dot("result", "vtable"), dot("this", "vtable")),
+      callThrough("this", "dup", @[dot("this", "obj"), dot("result", "obj")])
+    )
+  )
+  # =sink: destroy this, move from other (source) into this (dest), update vtable
+  result.add mkProc(
+    nnkAccQuoted.newTree(ident"=sink"),
+    @[newEmptyNode(), newIdentDefs(ident"this", nnkVarTy.newTree(ownedName)),
+      newIdentDefs(ident"other", ownedName)],
+    newStmtList(
+      callThrough("this", "destroy", @[dot("this", "obj")]),
+      callThrough("other", "sink", @[dot("other", "obj"), dot("this", "obj")]),
+      nnkAsgn.newTree(dot("this", "vtable"), dot("other", "vtable"))
     )
   )
 
+  # Converter: OwnedXxx -> Xxx
+  result.add mkConverter(
+    nnkPostfix.newTree(ident"*", ident"asPtr"),
+    @[name, newIdentDefs(ident"this", ownedName)],
+    newStmtList(nnkCast.newTree(name, ident"this"))
+  )
 
 
 macro implementInterfaceFor*(name: typed, implementors: varargs[typed]) =
-  ##
+  let nameStr = name.strVal
+  # Navigate to VtableXxx via the wrapper type's vtable field:
+  # getImpl(Xxx) -> TypeDef[2]=ObjectTy[2]=RecList, field[0]=vtable IdentDefs
+  # vtable type = PtrTy[VtableXxx], so [1][0] gives the VtableXxx sym
+  let wrapperImpl = name.getImpl
+  let vtableTypeSym = wrapperImpl[2][2][0][1][0]  # ptr VtableXxx -> VtableXxx sym
+  let vtableImpl = vtableTypeSym.getImpl
+  # TypeDef[2] = ObjectTy, ObjectTy[2] = RecList
+  let recList = vtableImpl[2][2]
 
+  const lifecycleNames = ["typenameHash", "destroy", "trace", "dup", "sink"]
+
+  # Extract interface method signatures from the vtable RecList
+  var methods: seq[MethodSig]
+  for fieldDef in recList:
+    let nameNode = fieldDef[0]
+    let fname = (if nameNode.kind == nnkPostfix: nameNode[1] else: nameNode).strVal
+    if fname in lifecycleNames: continue
+
+    let procType = fieldDef[1]
+    let formalParams = procType[0]
+    let retType = formalParams[0]
+
+    var nonThisParams: seq[NimNode]
+    for i in 1 ..< formalParams.len:
+      let p = formalParams[i]
+      if p[0].strVal != "this":
+        nonThisParams.add(nnkIdentDefs.newTree(p[0], p[1], newEmptyNode()))
+
+    methods.add(MethodSig(name: fname, nonThisParams: nonThisParams, retType: retType))
+
+  result = nnkStmtList.newTree()
+
+  let raisesNone = nnkPragma.newTree(ident"nimcall",
+    nnkExprColonExpr.newTree(ident"raises", nnkBracket.newTree()))
+  let nimcall = nnkPragma.newTree(ident"nimcall")
+
+  proc mkLambda(fParams: seq[NimNode], retType: NimNode, pragma: NimNode,
+                body: NimNode): NimNode =
+    nnkLambda.newTree(
+      newEmptyNode(), newEmptyNode(), newEmptyNode(),
+      nnkFormalParams.newTree(@[retType] & fParams),
+      pragma, newEmptyNode(), body
+    )
+
+  for impl in implementors:
+    let implStr = impl.strVal
+    let vtableConstName = ident("vtable_" & implStr & "_" & nameStr)
+
+    proc ptrImpl(): NimNode = nnkPtrTy.newTree(impl.copy)
+    proc derefCast(x: NimNode): NimNode =
+      nnkDerefExpr.newTree(nnkCast.newTree(ptrImpl(), x))
+
+    var objConstr = nnkObjConstr.newTree(ident("Vtable" & nameStr))
+
+    objConstr.add nnkExprColonExpr.newTree(
+      ident"typenameHash",
+      newCall(bindSym("hash"), newLit(implStr))
+    )
+
+    objConstr.add nnkExprColonExpr.newTree(ident"destroy",
+      mkLambda(
+        @[newIdentDefs(ident"this", ident"pointer")],
+        newEmptyNode(), raisesNone.copy,
+        newStmtList(newCall(nnkAccQuoted.newTree(ident"=destroy"), derefCast(ident"this")))
+      )
+    )
+
+    objConstr.add nnkExprColonExpr.newTree(ident"trace",
+      mkLambda(
+        @[newIdentDefs(ident"this", ident"pointer"),
+          newIdentDefs(ident"env", ident"pointer")],
+        newEmptyNode(), raisesNone.copy,
+        newStmtList(newCall(nnkAccQuoted.newTree(ident"=trace"),
+          derefCast(ident"this"), ident"env"))
+      )
+    )
+
+    # dup: this=source, other=dest — allocate and copy
+    objConstr.add nnkExprColonExpr.newTree(ident"dup",
+      mkLambda(
+        @[newIdentDefs(ident"this", ident"pointer"),
+          newIdentDefs(ident"other", nnkVarTy.newTree(ident"pointer"))],
+        newEmptyNode(), raisesNone.copy,
+        newStmtList(
+          nnkAsgn.newTree(ident"other",
+            newCall(ident"alloc0", newCall(ident"sizeof", impl.copy))),
+          nnkAsgn.newTree(derefCast(ident"other"), derefCast(ident"this"))
+        )
+      )
+    )
+
+    # sink: this=source, other=dest — allocate and move
+    objConstr.add nnkExprColonExpr.newTree(ident"sink",
+      mkLambda(
+        @[newIdentDefs(ident"this", ident"pointer"),
+          newIdentDefs(ident"other", nnkVarTy.newTree(ident"pointer"))],
+        newEmptyNode(), raisesNone.copy,
+        newStmtList(
+          nnkAsgn.newTree(ident"other",
+            newCall(ident"alloc0", newCall(ident"sizeof", impl.copy))),
+          nnkAsgn.newTree(derefCast(ident"other"),
+            newCall(ident"move", derefCast(ident"this")))
+        )
+      )
+    )
+
+    # Interface methods: delegate to the implementor's method
+    for m in methods:
+      var lambdaParams: seq[NimNode] = @[newIdentDefs(ident"this", ident"pointer")]
+      var callArgs: seq[NimNode]
+      for p in m.nonThisParams:
+        # p[0] is a sym from getImpl — use a fresh ident so it's the lambda's own param
+        let pname = ident(p[0].strVal)
+        lambdaParams.add(nnkIdentDefs.newTree(pname, p[1], newEmptyNode()))
+        callArgs.add(pname)
+
+      var methodExpr: NimNode
+      let access = nnkDotExpr.newTree(derefCast(ident"this"), ident(m.name))
+      if callArgs.len == 0:
+        methodExpr = access
+      else:
+        methodExpr = nnkCall.newTree(access)
+        for a in callArgs: methodExpr.add(a)
+
+      objConstr.add nnkExprColonExpr.newTree(ident(m.name),
+        mkLambda(lambdaParams, m.retType, nimcall.copy, newStmtList(methodExpr))
+      )
+
+    result.add nnkConstSection.newTree(
+      nnkConstDef.newTree(
+        nnkPostfix.newTree(ident"*", vtableConstName),
+        newEmptyNode(),
+        objConstr
+      )
+    )
+
+    # converter toXxx*(this: Impl): Xxx — unowned borrow
+    result.add nnkConverterDef.newTree(
+      nnkPostfix.newTree(ident"*", ident("to" & nameStr)),
+      newEmptyNode(), newEmptyNode(),
+      nnkFormalParams.newTree(
+        ident(nameStr),
+        newIdentDefs(ident"this", impl.copy)
+      ),
+      nnkPragma.newTree(ident"inline"),
+      newEmptyNode(),
+      newStmtList(nnkObjConstr.newTree(
+        ident(nameStr),
+        nnkExprColonExpr.newTree(ident"vtable",
+          nnkAddr.newTree(ident("vtable_" & implStr & "_" & nameStr))),
+        nnkExprColonExpr.newTree(ident"obj",
+          nnkAddr.newTree(ident"this"))
+      ))
+    )
+
+    # proc toOwnedXxx*(this: sink Impl): OwnedXxx — moves impl onto the heap
+    result.add nnkProcDef.newTree(
+      nnkPostfix.newTree(ident"*", ident("toOwned" & nameStr)),
+      newEmptyNode(), newEmptyNode(),
+      nnkFormalParams.newTree(
+        ident("Owned" & nameStr),
+        newIdentDefs(ident"this", impl.copy)
+      ),
+      nnkPragma.newTree(ident"inline"),
+      newEmptyNode(),
+      newStmtList(
+        nnkAsgn.newTree(
+          nnkDotExpr.newTree(ident"result", ident"vtable"),
+          nnkAddr.newTree(ident("vtable_" & implStr & "_" & nameStr))
+        ),
+        nnkAsgn.newTree(
+          nnkDotExpr.newTree(ident"result", ident"obj"),
+          newCall(ident"alloc0", newCall(ident"sizeof", impl.copy))
+        ),
+        nnkAsgn.newTree(
+          nnkDerefExpr.newTree(nnkCast.newTree(
+            nnkPtrTy.newTree(impl.copy),
+            nnkDotExpr.newTree(ident"result", ident"obj")
+          )),
+          ident"this"
+        )
+      )
+    )
