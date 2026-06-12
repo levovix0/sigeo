e2676845aa2e240f1bc89d22721b25f5963a2ae9
levovix <xlevovix@gmail.com>
2026-06-13 00:08:34 +0300

    add: Bounds2 can be empty

--- src/sigeo/core/bounds.nim
+++ src/sigeo/core/bounds.nim
@@ -5,30 +5,36 @@ import ./[vectors, points]
 type
   Bounds2* = object
     ## axis-aligned 2d bounding box
+    empty*: bool = true
     min*, max*: Point2
 
 
 proc bounds2*(p: Point2): Bounds2 =
-  Bounds2(min: p, max: p)
+  Bounds2(empty: false, min: p, max: p)
 
 proc bounds2*(a, b: Point2): Bounds2 =
   Bounds2(
+    empty: false,
     min: point2(min(a.x, b.x), min(a.y, b.y)),
     max: point2(max(a.x, b.x), max(a.y, b.y)),
   )
 
 
 proc add*(box: var Bounds2, p: Point2) =
+  if box.empty:
+    box = bounds2(p)
+    return
   box.min = point2(min(box.min.x, p.x), min(box.min.y, p.y))
   box.max = point2(max(box.max.x, p.x), max(box.max.y, p.y))
 
 proc add*(box: var Bounds2, other: Bounds2) =
+  if other.empty: return
   box.add other.min
   box.add other.max
 
-proc union*(a, b: Bounds2): Bounds2 =
-  result = a
-  result.add b
+proc `+`*(box: Bounds2, other: Bounds2): Bounds2 =
+  result = box
+  result.add other
 
 
 proc size*(box: Bounds2): V2 =
@@ -40,6 +46,9 @@ proc center*(box: Bounds2): Point2 =
 
 proc expanded*(box: Bounds2, amount: Float): Bounds2 =
   Bounds2(min: box.min - v2(amount, amount), max: box.max + v2(amount, amount))
+
+proc expanded*(box: Bounds2, margin: V2): Bounds2 =
+  Bounds2(min: box.min - margin, max: box.max + margin)
 
 
 proc overlaps*(a, b: Bounds2, tolerance: Float = epsilon(Float)): bool =
